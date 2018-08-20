99d53485aa95c8922804c6eeaa495cc79ef99e69
Tharindu <tharinduw@eight25media.com>
2018-08-20 21:54:50 +0530

    initial commit

---
+++ app/code/BenMarks/Brands/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tharindu
+ * Date: 8/18/18
+ * Time: 5:08 PM
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Benmarks_Brands',
+    __DIR__
+);
